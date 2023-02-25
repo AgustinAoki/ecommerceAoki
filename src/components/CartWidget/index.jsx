08afc07fb6c5fc499f6348699821fbd65a1ca087
agustinheraclio <agustinheraclio@gmail.com>
2023-02-25 07:54:10 -0300

    navbar

---
+++ src/components/CartWidget/index.jsx
@@ -0,0 +1,8 @@
+import './CartWidget.css';
+export default function CartWidget() {
+  return (
+    <div>
+      <i class='bi bi-cart-check-fill'>4</i>
+    </div>
+  );
+}
---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import CartWidget from '../CartWidget';
+import './NavBar.css';
+
+export default function NavBAr() {
+  return (
+    <div>
+      <Navbar collapseOnSelect fixed='top' bg='dark' variant='dark'>
+        <Container className='fondo'>
+          <Navbar.Brand href='#home'>Ecommerce</Navbar.Brand>
+          <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+          <Navbar.Collapse id='responsive-navbar-nav'>
+            <Nav className='me-auto'>
+              <Nav.Link href='#features'>Productos</Nav.Link>
+              <Nav.Link href='#pricing'>Tarjetas de Debito</Nav.Link>
+              <NavDropdown title='Cuenta' id='collasible-nav-dropdown'>
+                <NavDropdown.Item href='#action/3.1'>Wallet</NavDropdown.Item>
+                <NavDropdown.Item href='#action/3.2'>
+                  Ultimas compras
+                </NavDropdown.Item>
+                <NavDropdown.Item href='#action/3.3'>
+                  Seguridad
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href='#action/3.4'>
+                  Domicilios
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Nav>
+              <Nav.Link href='#deets'>Mi perfil</Nav.Link>
+              <Nav.Link eventKey={2} href='#memes'>
+                <CartWidget />
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+}
